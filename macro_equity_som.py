4b0ccb25a88f2f8856eec02c09ff726c47457e12
rdkdse <rickpaul.dhindsa@kimmeridge.com>
2023-02-14 14:43:20 -0500

    + Refactor to different classes

--- macro_equity_som.py
+++ macro_equity_som.py
@@ -46,134 +46,14 @@ from matplotlib import patches as patches
 #
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.datasets import make_blobs
+
 #
-from abc import ABC, abstractmethod, abstractproperty
-#
-
-# %% ---------------------------------- Constants
-
-DATA_DIMENSION = 3
-SPREAD_FACTOR = 0.01 # Value between 0 and 1
-GROWTH_THRESHOLD = -DATA_DIMENSION*np.log(SPREAD_FACTOR)
-
-# %% ---------------------------------- Helper Functions
-
-def scale_data_min_max_columnar(X):
-    min_ = np.min(X, axis=0)
-    max_ = np.max(X, axis=0)
-    X = (X - min_)/(max_ - min_)
-    scaling_dict = {"min_": min_, "max_": max_}
-    return(X, scaling_dict)
-
-def scale_data_min_max_global(X):
-    min_ = np.min(X)
-    max_ = np.max(X)
-    X = (X - min_)/(max_ - min_)
-    scaling_dict = {"min_": min_, "max_": max_}
-    return(X, scaling_dict)
-
-def de_scale_data_min_max(X, scaling_dict):
-    max_ = scaling_dict["max_"]
-    min_ = scaling_dict["min_"]
-    X = X * (max_ - min_) + min_
-    return(X)
 
 # %% ---------------------------------- Classes
 
-class abstract_SOM(ABC):
-    
-    @abstractproperty
-    def learning_rate(self):
-        pass
-    
-    @abstractproperty
-    def n_radius(self):
-        pass
-
-    @abstractmethod
-    def _fit_SOM(self):
-        pass
-    
-    @abstractmethod
-    def _scale_data(self, X):
-        pass
-    
-    @abstractmethod
-    def _descale_data(self, X):
-        pass
-    
-    def _update_dataset(self, X):
-        self._X, self._scaling_params = self._scale_data(X)
-        self.training_size, self.num_dims = self._X.shape
-    
-    def _update_architecture(self):
-        num_nodes = 5*math.ceil(math.sqrt(self.training_size))
-        self.SOM_size = math.ceil(math.sqrt(num_nodes))
-        self.vecs = np.random.uniform(low=0, high=1, size=(self.SOM_size**2, self.num_dims))
-        self.locs = [[i, j] for i in range(self.SOM_size) for j in range(self.SOM_size)]
-        # Initialize iterations
-        self.max_iter = self.SOM_size**2 * 500
-        self.curr_iter = 0 # TODO: I don't think we need this
-    
-    def _find_neighbors(self, bmu_idx):
-        bmu_loc = self.locs[bmu_idx]
-        if(self.manhattan_distance):
-            dists = np.sum(np.abs(np.array(self.locs) - bmu_loc), axis=1)
-        else:
-            dists = np.linalg.norm(np.array(self.locs) - bmu_loc, axis=1) # Euclidean distance
-        return(dists <= self.n_radius)
-    
-    def _find_bmu(self, x):
-        dists = np.linalg.norm(self.vecs - x, axis=1)
-        idx_ = np.argmin(dists)
-        return(idx_, dists[idx_])
-            
-    def fit(self, X):
-        self._update_dataset(X)
-        self._update_architecture()
-        self._fit_SOM()
-        self.X_hat = self._descale_data(self.vecs, self._scaling_params)
 
 # %% ---------------------------------- Basic SOM
 
-class Basic_SOM(abstract_SOM):
-    """
-    Reference:
-    https://towardsdatascience.com/understanding-self-organising-map-neural-network-with-python-code-7a77f501e985
-    """
-    
-    def __init__(self,
-                 learning_rate=0.5,
-                 neighborhood_radius=4,
-                 manhattan_distance=True,
-                 ):
-        self.init_learning_rate = learning_rate
-        self.init_n_radius = neighborhood_radius
-        self.manhattan_distance = manhattan_distance
-
-    @property
-    def learning_rate(self):
-        return(self.init_learning_rate * np.exp(-self.curr_iter/self.max_iter))
-    
-    @property
-    def n_radius(self):
-        return(self.init_n_radius * np.exp(-self.curr_iter/self.max_iter))
-
-    def _scale_data(self, X):
-        return scale_data_min_max_columnar(X)
-    
-    def _descale_data(self, X, scaling_dict):
-        return de_scale_data_min_max(X, scaling_dict)
-
-    def _fit_SOM(self):
-        for iter in range(self.max_iter):
-            if(iter % 1000 == 0):
-                print(f"Iteration: {iter}")
-            idx = np.random.randint(self.training_size)
-            bmu, bmu_dist = self._find_bmu(self._X[idx])
-            neighbors = np.where(self._find_neighbors(bmu))[0]
-            self.vecs[neighbors] += self.learning_rate * (self._X[idx] - self.vecs[neighbors]) ###### This is the key line in question
-            self.curr_iter = iter # TODO: I don't think we need this
 
 
 # %% ---------------------------------- Testing Basic SOM
